d1ed69a9026dca9238dfe2c450d1eb297c263888
moto <motochan1969@gmail.com>
2021-05-19 08:45:39 +0900

    update

--- crawl.py
+++ crawl.py
@@ -8,15 +8,13 @@ from alpha_vantage.timeseries import TimeSeries
 from time import sleep
 
 api_key = 'KDDTQTNE9OTMD4V4.'
-symbols = ['EURUSD','GBPJPY','GBPUSD','NZDJPY','USDCAD','USDJPY','EURGBP','EURJPY','GBPAUD']
+symbols = ['RGLDOU.SWI','EURUSD','GBPJPY','GBPUSD','NZDJPY','USDCAD','USDJPY','EURGBP','EURJPY','GBPAUD']
 
 time_window = 'daily'
 
 def main():
     ts = TimeSeries(key=api_key, output_format='pandas')
-    for symbol_sep in symbols:
-        for symbol in [symbol_sep,symbol_sep[3:]+symbol_sep[:3]]:
-            sleep(2)
+    for symbol in symbols:
             try:
                 data, meta_data = ts.get_daily(symbol.lower(), outputsize='full')
                 data.sort_values('date',inplace=True,ascending=False)
